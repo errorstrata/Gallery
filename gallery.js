35c76b13c5de5b51cdc8a6ad5032cae1da4904cf
errorstrata <jonskevins0@gmail.com>
2025-12-09 17:51:11 +0300

    12

--- gallery.js
+++ gallery.js
@@ -14,24 +14,22 @@ function setCurrent(activeBtn) {
   activeBtn.classList.add('current');
 }
 
-let board = document.querySelector('.body')
-
 function loadImages() {
  fetch('http://127.0.0.1:5000/files')
  .then(res => res.json())
  .then(data => {
   let images = data.files;
   images.forEach(image => {
-    let imageType = image.type;
-    if (imageType.startsWith('image/')) {
+    let fileType = image.type;
+    if (fileType.startsWith('image/')) {
       const img = document.createElement('img');
-      img.src = `http://127.0.0.1:5000/files/${image}ˋ;
+      img.src = `http://127.0.0.1:5000/files/${image}`;
       img.classList.add('image')
-      imageview_box.appendChild(image)
+      imageview_box.appendChild(img)
     }
     else if(fileType.startsWith('video/')) {
       const video = document.createElement('video');
-      video.src = ˋhttp://127.0.0.1:5000/files/${video}ˋ
+      video.src = `http://127.0.0.1:5000/files/${image}`;
       video.classList.add('video')
       videoview_box.appendChild(video)
     }
