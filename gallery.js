63dacafa8fa7b6121d33a62296b37cfc9634be6f
errorstrata <jonskevins0@gmail.com>
2025-12-10 11:06:37 +0300

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -21,17 +21,18 @@ function loadImages() {
  .then(res => res.json())
  .then(data => {
   let images = data.files;
-  images.forEach(image => {
+  images.forEach(item => {
+    let image = `http://127.0.0.1:5000/files/${item}`
     let fileType = image.type;
     if (fileType.startsWith('image/')) {
       const img = document.createElement('img');
-      img.src = `http://127.0.0.1:5000/files/${image}`;
+      img.src = image;
       img.classList.add('photo')
       imageview_box.appendChild(img)
     }
     else if(fileType.startsWith('video/')) {
       const video = document.createElement('video');
-      video.src = `http://127.0.0.1:5000/files/${image}`;
+      video.src = image;
       video.classList.add('photo')
       videoview_box.appendChild(video)
     }
